aaf2977170aebc05510c966868d40a774505c21c
Anwesh43 <anweshthecool0@gmail.com>
2019-07-14 11:38:21 +0530

    adding constants and libraries

--- halfarcstepbarview/src/main/java/com/anwesh/uiprojects/halfarcstepbarview/HalfArcStepBarView.kt
+++ halfarcstepbarview/src/main/java/com/anwesh/uiprojects/halfarcstepbarview/HalfArcStepBarView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.halfarcstepbarview
 /**
  * Created by anweshmishra on 14/07/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.RectF
+import android.content.Context
+
+val nodes : Int = 5
+val bars : Int = 4
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#283593")
+val backColor : Int = Color.parseColor("#BDBDBD")
