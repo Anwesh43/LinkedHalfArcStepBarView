93ac84c8b69b080e1d9d78f85dc2b1b1be4be253
Anwesh43 <anweshthecool0@gmail.com>
2019-07-14 12:03:28 +0530

    adding Canvas extension method

--- halfarcstepbarview/src/main/java/com/anwesh/uiprojects/halfarcstepbarview/HalfArcStepBarView.kt
+++ halfarcstepbarview/src/main/java/com/anwesh/uiprojects/halfarcstepbarview/HalfArcStepBarView.kt
@@ -9,6 +9,7 @@ import android.view.MotionEvent
 import android.app.Activity
 import android.graphics.Paint
 import android.graphics.Color
+import android.graphics.Canvas
 import android.graphics.RectF
 import android.content.Context
 
@@ -30,3 +31,39 @@ fun Float.mirrorValue(a : Int, b : Int) : Float {
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
+
+fun Canvas.drawHalfArc(scale : Float, size : Float, paint : Paint) {
+    drawArc(RectF(-size, -size, size, size), 0f, 180f * scale, true, paint)
+}
+
+fun Canvas.drawBar(i : Int, sc : Float, size : Float, paint : Paint) {
+    val wBar : Float = (2 * size) / (2 * bars + 1)
+    val sci : Float = sc.divideScale(i, bars)
+    save()
+    translate(wBar * (i + 1), 0f)
+    drawRect(RectF(-wBar / 2, -size * sci, wBar / 2, 0f), paint)
+    restore()
+}
+
+fun Canvas.drawBars(size : Float, sc : Float, paint : Paint) {
+    for (j in 0..(bars - 1)) {
+        drawBar(j, sc, size, paint)
+    }
+}
+
+fun Canvas.drawHASBNode(i : Int, scale : Float, paint : Paint) {
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    paint.color = foreColor
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    save()
+    translate(w / 2, gap * (i + 1))
+    drawBars(size, sc2, paint)
+    drawHalfArc(sc1, size, paint)
+    restore()
+}
