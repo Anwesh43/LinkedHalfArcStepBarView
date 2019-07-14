6ee3b4e24ef4beceaf501f686477b1721d745738
Anwesh43 <anweshthecool0@gmail.com>
2019-07-14 13:05:33 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedhalfarcstepbarview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedhalfarcstepbarview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedhalfarcstepbarview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.halfarcstepbarview.HalfArcStepBarView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        HalfArcStepBarView.create(this)
     }
 }
